a1f519c8cefaad43f1836ad7dd5bad777c051b5c
StefanoMela <stefano.mela25@gmail.com>
2023-10-17 14:24:57 +0200

    gfx fix

--- resources/views/train/index.blade.php
+++ resources/views/train/index.blade.php
@@ -6,11 +6,11 @@
   @forelse($trains as $train)
   <div class="card my-3 w-100">
     <div class="card-body">
-      <p class="card-text"><strong> Compagnia:</strong> {{ $train->company }}</p>
-      <p class="card-text"><strong> Stazione di Partenza:</strong>{{ $train->departure_station }}</p>
-      <p class="card-text"><strong> Stazione di Arrivo:</strong>{{ $train->arrival_station }}</p>
-      <p class="card-text"><strong> Orario di Partenza:</strong>{{ $train->departure_time }}</p>
-      <p class="card-text"> <strong>Orario di Arrivo:</strong>{{ $train->arrival_time }}</p>
+      <p class="card-text"><strong>Compagnia: </strong> {{ $train->company }}</p>
+      <p class="card-text"><strong>Stazione di Partenza: </strong>{{ $train->departure_station }}</p>
+      <p class="card-text"><strong>Stazione di Arrivo: </strong>{{ $train->arrival_station }}</p>
+      <p class="card-text"><strong>Orario di Partenza: </strong>{{ $train->departure_time }}</p>
+      <p class="card-text"> <strong>Orario di Arrivo: </strong>{{ $train->arrival_time }}</p>
       @if (@$train->train_on_time)
       <p class="card-text"><strong>È in orario ? </strong> Si, partirà il giorno : {{ $train->arrival_time }}</p>
       @else
